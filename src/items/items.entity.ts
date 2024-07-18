b280c52e92cef078da43369d8d4736fd2fb59c1e
Animatere <alexwolf822@yahoo.de>
2024-07-18 14:01:04 +0200

    project init

---
+++ src/items/items.entity.ts
@@ -0,0 +1,28 @@
+import * as mongoose from 'mongoose';
+
+export enum DishType {
+  PIZZA = 'Pizza',
+  PASTA = 'Pasta',
+  DESSERT = 'Dessert',
+  SIDE_DISH = 'Side Dish',
+  SALAD = 'Salad',
+  SOUP = 'Soup',
+  SNACK = 'Snack',
+  BREAKFAST = 'Breakfast',
+  LUNCH = 'Lunch',
+  DINNER = 'Dinner',
+  CUSTOM = 'Custom',
+  BURGER = 'Burger',
+}
+
+export const ItemSchema = new mongoose.Schema({
+  incredience: { type: [String], required: true },
+  description: { type: String, required: true },
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  link: { type: String, required: true },
+  dishType: { type: String, enum: Object.values(DishType), required: true },
+});
+
+export default mongoose.model('Item', ItemSchema);
